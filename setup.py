82ef9c0eba8056e1c802e96b3bbc159fda69cbb8
Ryan S. DeFever <r.s.defever@gmail.com>
2020-03-26 16:51:30 -0400

    Remove copied material :(

--- setup.py
+++ setup.py
@@ -18,35 +18,6 @@ requirements = [
 ]
 
 setup(
-    name='gmso',
-    version=__version__,
-    packages=find_packages(),
-    zip_safe=True,
-    author='Matthew W Thompson, Justin Gilmer',
-    author_email='matt.thompson@vanderbilt.edu, justin.b.gilmer@vanderbilt.edu',
-    url='https://github.com/mosdef-hub/gmso',
-    download_url='https://github.com/mosdef-hub/gmso/tarball/{}'.format(__version__),
-    license="MIT",
-    keywords='gmso',
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-        'Operating System :: MacOS',
-    ],
-    install_requires=requirements,
-    python_requires='>=3.6, <4',
-)
-
-setup(
     name="mosdef_cassandra",
     version=__version__,
     packages=find_packages(),

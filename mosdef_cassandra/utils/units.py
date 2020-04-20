ee78926d32892d2389a97a767540f48ee740adf0
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-04-20 16:28:39 -0500

    Rename nested function within validate_unit

--- mosdef_cassandra/utils/units.py
+++ mosdef_cassandra/utils/units.py
@@ -4,10 +4,10 @@ from unyt import accepts
 
 def validate_unit(unyt_array, dimension):
     @accepts(unyt_array=dimension)
-    def validate_unit(unyt_array):
+    def _validate(unyt_array):
         if isinstance(unyt_array, u.unyt_array):
             return unyt_array
         else:
             raise TypeError("Argument must be a unyt_array")
 
-    return validate_unit(unyt_array)
+    return _validate(unyt_array)

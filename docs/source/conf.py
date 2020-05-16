c536e775874173b15d38c42626d03d402820b032
Ryan S. DeFever <r.s.defever@gmail.com>
2020-05-16 14:21:55 -0400

    Set RTD version to latest

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2020, University of Notre Dame"
 author = "Ryan S. DeFever"
 
 # The short X.Y version
-version = "0.1"
+version = "latest"
 # The full version, including alpha/beta/rc tags
 release = ""
 

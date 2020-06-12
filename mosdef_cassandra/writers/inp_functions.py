c59b3c0e4225905cf9807aafe60c262a4f62fb71
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-06-12 14:25:56 -0500

    Add unit test to check conversion of restricted insertion values

--- mosdef_cassandra/writers/inp_functions.py
+++ mosdef_cassandra/writers/inp_functions.py
@@ -982,17 +982,19 @@ def get_box_info(boxes, restricted_type, restricted_value):
             for typ, value in zip(restrict_types, restrict_vals):
                 _check_restricted_insertions(box, typ, value)
                 if typ == "interface":
-                    value[0] = value[0].to("angstrom")
-                    value[1] = value[1].to("angstrom")
+                    # value[0] = value[0].to("angstrom")
+                    # value[1] = value[1].to("angstrom")
                     inp_data += """restricted_insertion {} {} {}
                     """.format(
-                        typ, value[0].to_value(), value[1].to_value()
+                        typ,
+                        value[0].to("angstrom").to_value(),
+                        value[1].to("angstrom").to_value(),
                     )
                 elif typ:
-                    value = value.to("angstrom")
+                    # value = value.to("angstrom")
                     inp_data += """restricted_insertion {} {}
                     """.format(
-                        typ, value.to_value()
+                        typ, value.to("angstrom").to_value()
                     )
 
     else:

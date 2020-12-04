b45ae85491afc7942924131a1d682b7337fc862a
Ryan S. DeFever <r.s.defever@gmail.com>
2020-12-04 11:33:54 -0500

    Fix version dunder in __init__.py and add to bumpversion.

--- mosdef_cassandra/__init__.py
+++ mosdef_cassandra/__init__.py
@@ -7,4 +7,4 @@ from .runners.runners import restart
 from .writers.inp_functions import print_valid_kwargs
 from .writers.writers import print_inputfile
 
-__version__ = "0.2.1"
+__version__ = "0.2.3"
